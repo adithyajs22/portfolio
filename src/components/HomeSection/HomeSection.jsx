93bb2f8755084b01544e58b9c3f9875f5e4472c8
adithyajs <adithyajs2210@gmail.com>
2026-09-08 14:01:25 +0530

    new project added

--- src/components/HomeSection/HomeSection.jsx
+++ src/components/HomeSection/HomeSection.jsx
@@ -10,7 +10,7 @@ const HomeSection = () => {
     <div className="hero-content">
       <h1>Hello</h1>
       <h2>I'm <span>Adithya J S</span></h2>
-      <h1  className="right-floating-label"><strong>Frontend Developer & Tech Enthusiast</strong> </h1>
+      <h2 className="subtitle">Frontend Developer & Tech Enthusiast</h2>
       <p>I'm a Computer Science Graduate & Frontend Engineer. </p>
       <p> I design and build modern, highly accessible web applications using React and Vite, with a relentless focus on clean code and usability. Actively seeking full-time roles and open to new opportunities.</p>
       <div className='herocontent'>
